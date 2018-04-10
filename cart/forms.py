745827dc88b08392647a912add8111a78a48af83
Williano <paawilly17@gmail.com>
2018-04-10 19:49:17 +1200

    Update of Quantity of items

--- cart/forms.py
+++ cart/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 101)]
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
